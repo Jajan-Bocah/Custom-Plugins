5e613daa228781b927f9cce663bb77553895010f
Jajan-Bocah <jajan.bocah@gmail.com>
2022-03-26 12:23:13 +0700

    Removed magisk from android plugins

--- plugins/utils/android/__main__.py
+++ plugins/utils/android/__main__.py
@@ -42,31 +42,3 @@ async def device_recovery(message: Message):
         f"**Updated:** __{date}__"
     )
     await message.edit(reply)
-
-
-@userge.on_cmd("magisk$", about={"header": "Get Latest Magisk Zip and Manager"})
-async def magisk_(message: Message):
-    """Get Latest MAGISK"""
-    magisk_repo = "https://raw.githubusercontent.com/topjohnwu/magisk_files/"
-    magisk_dict = {
-        "⦁ 𝗦𝘁𝗮𝗯𝗹𝗲": magisk_repo + "master/stable.json",
-        "⦁ 𝗕𝗲𝘁𝗮": magisk_repo + "master/beta.json",
-        "⦁ 𝗖𝗮𝗻𝗮𝗿𝘆": magisk_repo + "canary/canary.json",
-    }
-    releases = "<code><i>𝗟𝗮𝘁𝗲𝘀𝘁 𝗠𝗮𝗴𝗶𝘀𝗸 𝗥𝗲𝗹𝗲𝗮𝘀𝗲:</i></code>\n\n"
-    for name, release_url in magisk_dict.items():
-        data = get(release_url).json()
-        if "canary" in release_url:
-            data["app"]["link"] = magisk_repo + "canary/" + data["app"]["link"]
-            data["magisk"]["link"] = magisk_repo + "canary/" + data["magisk"]["link"]
-            data["uninstaller"]["link"] = (
-                magisk_repo + "canary/" + data["uninstaller"]["link"]
-            )
-
-        releases += (
-            f'{name}: [ZIP v{data["magisk"]["version"]}]({data["magisk"]["link"]}) | '
-            f'[APK v{data["app"]["version"]}]({data["app"]["link"]}) | '
-            f'[Uninstaller]({data["uninstaller"]["link"]})\n'
-        )
-
-    await message.edit(releases, disable_web_page_preview=True)
